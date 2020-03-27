128228d42eba418bf520f0454c046b6d1ffe03ac
Danilo <daniloarpesam@gmail.com>
2020-03-27 18:50:21 -0300

    added integration with robofriend, and searchBox component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { robots } from './robots';
+import CardList from './Card.list.jsx';
+import SearchBox from './SearchBox.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      robots,
+      searchField: '',
+    };
+  }
+
+  onSearchChange(event) {
+    console.log(event);
+  }
+
+  render() {
+    console.log('this --', this)
+    return (
+      <div className='tc'>
+        <h1>RoboFriends</h1>
+        <SearchBox onSearchChange={this.onSearchChange} />
+        <CardList robots={this.state.robots} />
+      </div>
+    )
+  }
+};
 
 export default App;
